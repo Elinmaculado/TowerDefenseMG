ffcec85b74f627e7996921954cbc38f3d5580d74
Juan Haro <jcharomo@gmail.com>
2023-12-26 13:36:54 -0600

    Main menu

---
+++ Assets/scrpts/GameFlow/LevelManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
