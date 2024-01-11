6c91240857f49efd374e929735dc6b6397bc85f7
Jared Osorno <jaredlares5@gmail.com>
2024-01-10 18:50:25 -0600

    pequeño setup

--- Assets/scrpts/Towers/Tower_Upgrader/Tower_Upgrader.cs
+++ Assets/scrpts/Towers/Tower_Upgrader/Tower_Upgrader.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Tower_Upgrader : MonoBehaviour
 {
+    [SerializeField] private List<int> Range;
+    [SerializeField] private List<int> AttackSpeed;
+    [SerializeField] private List<int> Damage;
+    [SerializeField] private List<int> ammoSpeed;
+    [SerializeField] private int topUpgradeTier;
+    Tower_Stats ts;
     // aqui va a ver codigo
+
+    private void Start()
+    {
+        ts= GetComponent<Tower_Stats>();
+    }
 
 }
