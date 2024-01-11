fc686e6627c7c809ad491185ae1ad7f8190ba3d7
Jared Osorno <jaredlares5@gmail.com>
2024-01-10 20:22:50 -0600

    acomodo de mejoras de forma extremadamente burda

--- Assets/scrpts/Towers/Tower_Upgrader/Tower_Upgrader.cs
+++ Assets/scrpts/Towers/Tower_Upgrader/Tower_Upgrader.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class Tower_Upgrader : MonoBehaviour
@@ -10,7 +11,9 @@ public class Tower_Upgrader : MonoBehaviour
     [SerializeField] private List<int> Damage;
     [SerializeField] private List<int> ammoSpeed;
     [SerializeField] private int topUpgradeTier;
+    [SerializeField] private int downUpgradeTier;
     Tower_Stats ts;
+
     // aqui va a ver codigo
 
     private void Start()
@@ -18,4 +21,29 @@ public class Tower_Upgrader : MonoBehaviour
         ts= GetComponent<Tower_Stats>();
     }
 
+    public void topUpgrade()
+    {
+        if(downUpgradeTier <=2) 
+        {
+            Debug.Log(" el camino esta bloqueado");
+        }
+        else
+        {
+            ts.TowerRange += Range[topUpgradeTier];
+            ts.ammoSpeed += ammoSpeed[topUpgradeTier];
+        }
+    }
+
+    public void downUpgrade()
+    {
+        if(topUpgradeTier <=2)
+        {
+            Debug.Log("camino bloqueado");
+        }
+        else
+        {
+            ts.TowerDamage += Damage[downUpgradeTier]; 
+            ts.TowerAttackSpeed += AttackSpeed[downUpgradeTier];
+        }
+    }
 }
