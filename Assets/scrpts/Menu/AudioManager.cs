ce5e06290e2a69357522bf1c04c751e325fad027
Juan Haro <jcharomo@gmail.com>
2024-01-15 15:34:33 -0600

    Zombies sounds added

--- Assets/scrpts/Menu/AudioManager.cs
+++ Assets/scrpts/Menu/AudioManager.cs
@@ -16,7 +16,7 @@ public class AudioManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
+            Destroy(gameObject);
         }
         else
         {
